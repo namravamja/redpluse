ed3bdb44c08f4fc45ef70b8c3c94bf768c810348
NamraVamja <namravamja00@gmail.com>
2025-05-11 21:31:24 +0530

    fix: Update base URL in BloodBank API to include '/api' suffix

--- src/app/lib/BloodBank.tsx
+++ src/app/lib/BloodBank.tsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "BloodBankApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   }),
   endpoints: (builder) => ({
     // Signup endpoint
