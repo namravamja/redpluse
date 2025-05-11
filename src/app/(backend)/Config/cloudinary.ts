a8736ccecaa7eaebf6a4bb6528e3407b216deedb
NamraVamja <namravamja00@gmail.com>
2025-05-12 01:35:57 +0530

    fix: Add environment variable checks for Cloudinary configuration and update CORS middleware to include profile photo endpoint

--- src/app/(backend)/Config/cloudinary.ts
+++ src/app/(backend)/Config/cloudinary.ts
@@ -1,5 +1,14 @@
 import { v2 as cloudinary } from "cloudinary";
 
+// Check if environment variables are present
+if (
+  !process.env.CLOUDINARY_CLOUD_NAME ||
+  !process.env.CLOUDINARY_API_KEY ||
+  !process.env.CLOUDINARY_API_SECRET
+) {
+  throw new Error("Missing Cloudinary environment variables");
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
   api_key: process.env.CLOUDINARY_API_KEY!,
